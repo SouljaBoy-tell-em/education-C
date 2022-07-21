45aea33d997764ea8f096ea5ebe6c087cc00eb69
sashascul <60592559+sashascul@users.noreply.github.com>
2022-07-21 18:28:24 +0600

    Add files via upload

---
+++ introduction1.cpp
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+#include <float.h>
+#include <limits.h>
+
+#define PRAISE "Your name is the best!"
+#define NAME value
+#define BEEP '\a'
+//#define LLONG_MAX
+#define PAGES 931
+
+
+int main (void) {
+	
+	//char name[40];
+	
+	const float a = 3852.99;
+	
+	const int B = 6;
+	
+	//printf("what is your name?\n");
+	
+	//scanf("%s", name);
+	
+	/*
+	printf("Hello, %s! %s\n", name, PRAISE);
+	printf("Your name contains %d simbols and take in memory %d.\n", strlen(name), sizeof name);
+	printf("Cool fraze contains %d simbols", strlen(PRAISE));
+	printf("and take in memory %d.\n", sizeof(PRAISE));
+	*/
+	
+	//printf("%s", NAME);
+	
+	//printf("%c", BEEP);
+	
+	//printf("%lld\n", LLONG_MAX);
+	
+	//printf("*%4.34f*\n", a);
+	
+	//printf("*%-4d*\n", PAGES);
+	
+	//printf("%20.10d", B);
+	
+	//int bph2o = 212;
+	
+	//int rv = printf("%d градусов по Фаренгейту соответствует точке кипения воды.\n", bph2o);
+	
+	//printf("%d", rv);
+	
+	/*
+	
+	printf("Один из способов ввода \
+	длинных строк\n");
+	
+	printf("Ещё один из способов ввода"
+	" длинных строк");
+	
+	
+	
+	int s;
+	
+	scanf("%d", &s);
+	
+	printf("%d", s);
+	
+	
+	
+	unsigned width;
+	int number = 256;
+	double weight = 242.5;
+	
+	*/
+	
+	
+}
