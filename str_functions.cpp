db4a60833597483c5f1a723119a27dce9eb1d869
Big Bill <60592559+sashascul@users.noreply.github.com>
2022-07-28 05:27:41 +0600

    Add files via upload

---
+++ str_functions.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+int puts1 (const char * ar);
+char* strchr1 (char * ar, int ch);
+
+int main () {
+	
+	
+	
+}
+
+int puts1 (const char * ar) {
+	
+	int i = 0;
+	
+	while (*(ar + i) != '\0') {
+	
+		putchar(*(ar + i));
+		i++;
+	
+	}
+	
+	if (*(ar + i) == '\0') {
+		putchar ('\n');
+		return 0;
+	}
+	else
+		return -1;
+	
+}
+
+char* strchr1 (char * ar, int ch) {
+	
+	int i = 0;
+	
+	while (*(ar + i) != '\0') {
+		
+		if (*(ar + i) == (char)ch)
+			break;
+		i++;
+				
+	}
+	
+	return (ar + i);
+	
+}
+
+
