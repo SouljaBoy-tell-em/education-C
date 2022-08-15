53353aa8d76f9beedbe2331657521e51428dc3ab
Big Bill <60592559+sashascul@users.noreply.github.com>
2022-08-15 06:48:15 +0600

    Update pro.c

--- pro.c
+++ pro.c
@@ -4,6 +4,8 @@
 
 #define TSIZE 45
 
+/*
+
 struct film {
 
   char title [TSIZE];
@@ -12,7 +14,42 @@ struct film {
 
 };
 
+*/
+
+struct film {
+
+  char title[TSIZE];
+  int rating;
+
+};
+
+typedef struct film Item;
+
+struct node {
+
+  Item item;
+  struct node * next;
+
+}
+
+typedef struct node Node;
+typedef Node * List;
+
+
+void InitializeList (List * plist);
+bool ListIsEmpty (const List * plist);
+bool ListIsFull (const List * plist);
+unsigned int ListItemCount (const List * plist);
+bool AddItem (Item item, List * plist);
+void Traverse (const List * plist, void (* pfun)(Item item));
+void EmptyTheList (const List * plist);
+void showmovies (Item item);
+static void CopyToNode (Item item, Node * pnode);
+
+
 int main (void) {
+
+  /*
 
   struct film * head = NULL;
   struct film * prev, * current;
@@ -72,5 +109,88 @@ int main (void) {
 
   puts ("End of program!");
 
+  */
+
+  List movies; // <=> struct node * movies;
+  Item temp;  //  <=> struct film temp;
+  InitializeList (&movies); // &movies можно записать в переменную вида:
+  // struct node ** value; (либо List * value);
+
+  if (ListIsFull (&movies) == true) {
+
+    puts ("Free memory not found");
+    exit (1);
+
+  }
+
+  puts ("Input title of 1st film:");
+
+  while (gets (temp.title) != NULL && temp.title[0] != '\0') {
+
+    puts ("Input your ecquation of rating: ");
+    scanf ("%d", &temp.rating);
+
+    while (getchar () != '\n')
+      continue;
+
+    if (AddItem (temp, &movies) == false) {
+
+      puts ("Problem with adding of memory");
+      exit (1);
+
+    }
+
+  }
+
   return 0;
+}
+
+void InitializeList (List * plist) {
+                //  (struct node ** plist) - указатель на указатель.
+  //  Нужно для для записи адреса и его изменения. Если записать так:
+  // List plist, то переменную нельзя будет
+
+
+  * plist = NULL; // фактически в ** plist был записан адрес адреса.
+  // * plist - разыменовывание и получение переменной типа адреса.
+  // адрес принимает NULL. Адрес на структуру нулевой, поэтому список
+  // инициализируется пустым содержимым.
+
+}
+
+
+
+bool AddItem (Item item, List * plist) {
+
+  Node * pnew; // создание указателя на структуру (struct node * pnew)
+  Node * scan = * plist; // создание указателя на структуру и присваивание
+  // адреса переменной movies.
+  pnew = (Node *) malloc (sizeof (Node)); // динамическое выделение памяти
+
+  if (pnew == NULL) // если pnew = NULL, то проблема с выделением памяти
+    return false;
+
+  CopyToNode (item, pnew); // в качестве переменной item была передана
+  // была передана temp - уже заполненная структура. temp скопирована
+  // в pnew.
+  pnew->next = NULL; // установка указателя в нулевое значение
+
+  if (scan == NULL) // если сохранение идёт впервые, то scan имеет нулевой указатель
+    * plist = pnew; // movies обновляет свой адрес. Новый адрес будет хранится в
+  // main () в теле. Потом по новой будет передан в scan (в начале)
+
+  else { // сохранение идёт не впервые
+
+    while (scan->next != NULL) // последовательное прихождение к адресу.
+      scan = scan->next;
+    scan->next = pnew; // в указатель присваивается адрес ??????????
+
+  }
+
+}
+
+static void CopyToNode (Item item, Node * pnode); {
+
+  pnode->item = item;
+
 }
