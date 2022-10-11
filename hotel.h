d6a046446e42d0b75c9a2b4bf13abcd7d8826d3d
Big Bill <60592559+sashascul@users.noreply.github.com>
2022-10-11 11:29:00 +0300

    Create hotel.h

---
+++ hotel.h
@@ -0,0 +1,12 @@
+#define QUIT 5
+#define HOTEL1 180.00
+#define HOTEL2 225.00
+#define HOTEL3 255.00
+#define HOTEL4 355.00
+#define DISCOUNT 0.95
+#define START "********************************\n"
+
+void HELP_NUM_HOTEL (void);
+int NUM_HOTEL (void);
+int nights (void);
+
